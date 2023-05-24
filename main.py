89c6396a803aa93cac5df18147312d36219d2fec
Ahmed Hafez <35895595+Ahmedtronic@users.noreply.github.com>
2023-05-24 03:05:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 from flask import Flask, jsonify
 import os
+from PIL import Image
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def is_valid_image(file):
+    try:
+        image = Image.open(file)
+        image.verify()
+        return True
+    except:
+        return False
 
+
+@app.route('/', methods=['POST'])
+def index():
+    image = Image.open("data/300605749_1496471350812876_7436557921197375791_n.jpg")
+    try:
+        image.verify()
+        print(str(image.filename))
+        return str(image.filename)
+    except Exception:
+        return "None"
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
+    
+
+
+    
+    
